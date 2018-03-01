a72f1cca73b823562cc24bea0761cec053677bd6
Hubert Sui <huberts@canviz.com>
2018-03-01 21:31:20 +0800

    remove humanize from downloader

--- cmd/extracting/downloader.go
+++ cmd/extracting/downloader.go
@@ -5,9 +5,6 @@ import (
 	"io"
 	"net/http"
 	"os"
-	"strings"
-
-	"github.com/dustin/go-humanize"
 )
 
 // WriteCounter counts the number of bytes written to it. It implements to the io.Writer
@@ -20,18 +17,7 @@ type WriteCounter struct {
 func (wc *WriteCounter) Write(p []byte) (int, error) {
 	n := len(p)
 	wc.Total += uint64(n)
-	wc.PrintProgress()
 	return n, nil
-}
-
-func (wc WriteCounter) PrintProgress() {
-	// Clear the line by using a character return to go back to the start and remove
-	// the remaining characters by filling it with spaces
-	fmt.Printf("\r%s", strings.Repeat(" ", 35))
-
-	// Return again and print current status of download
-	// We use the humanize package to print the bytes in a meaningful way (e.g. 10 MB)
-	fmt.Printf("\rDownloading... %s complete", humanize.Bytes(wc.Total))
 }
 
 func downloadFile(filepath string, url string) error {
